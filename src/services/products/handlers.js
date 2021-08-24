ca65154cd88734b680e372eb7dce641ed6cf5978
VardanGalstian <81989128+VardanGalstyan@users.noreply.github.com>
2021-08-24 20:50:52 +0200

    CRUD_Fully_implemented_with_SQL

--- src/services/products/handlers.js
+++ src/services/products/handlers.js
@@ -1,22 +1,23 @@
 import db from "../../db/connection.js"
 
 
-export const list = async (res, req, next) => {
+export const list = async (req, res, next) => {
     try {
         const products = await db.query(`SELECT * FROM products`)
         res.send(products.rows)
     } catch (error) {
-        console.log(error);
+        res.status(500).send(error)
     }
 };
 
-export const single = async (res, req, next) => {
+export const single = async (req, res, next) => {
     try {
         const { product_id } = req.params
-        const products = await db.query(`SELECT * FROM products WHERE product_id=${product_id}`)
-        res.send(products.rows[0])
+        const products = await db.query(`SELECT * FROM products WHERE product_id=${product_id};`);
+        const [found, ...rest ] = products.rows;
+        res.status(found ? 200 : 400).send(found)
     } catch (error) {
-        console.log(error);
+        res.status(500).send(error)
     }
 };
 
@@ -26,6 +27,41 @@ export const create = async (req, res, next) => {
         const products = await db.query(
             `INSERT INTO products(name, description, brand, image_url, price, category) VALUES('${name}', '${description}', '${brand}', '${image_url}', '${price}', '${category}') RETURNING *;`);
         res.send(products.rows[0])
+    } catch (error) {
+        res.status(500).send(error)
+    }
+};
+
+
+export const update = async (req, res, next) => {
+    try {
+        const {product_id} = req.params
+        const { name, description, brand, image_url, price, category } = req.body
+        const products = await db.query(
+            `UPDATE products
+             SET name = '${name}', 
+             description = '${description}',
+             brand = '${brand}',
+             image_url = '${image_url}',
+             price = '${price}',
+             category = '${category}',
+             updated_at = NOW() 
+             WHERE product_id = ${product_id} RETURNING *;`
+        );
+        const [found, ...rest] = products.rows;
+        res.status(found ? 200 : 400).send(found)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+};
+
+export const deleteProduct = async (req, res, next) => {
+    try {
+        const { product_id } = req.params
+        const rdResult = await db.query(
+            `DELETE FROM products WHERE product_id =${product_id}`
+        );
+        res.status(rdResult.rowCount? 200 : 400).send()
     } catch (error) {
         res.status(500).send(error)
     }
--- src/services/products/routes.js
+++ src/services/products/routes.js
@@ -6,5 +6,7 @@ const productsRoute = Router()
 productsRoute.get("/", productHandlers.list)
 productsRoute.get("/:product_id", productHandlers.single)
 productsRoute.post("/", productHandlers.create)
+productsRoute.put("/:product_id", productHandlers.update)
+productsRoute.delete("/:product_id", productHandlers.deleteProduct)
 
 export default productsRoute
